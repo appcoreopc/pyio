59dd9e1a4cadea17e04a43a66061666a92f1cfc0
jeremy <jeremy@jeremys-MacBook-Pro.local>
2020-06-14 15:02:19 +0800

    added lambda function into the code

--- core/csvreader.py
+++ core/csvreader.py
@@ -3,12 +3,15 @@ import csv
 class CsvFileReader:
     def __init__(self, name):
         self.filename = name
-    
-    def read(self, chunksize=1024):
+
+    def read(self, function, chunksize=1024):
         with open(self.filename, "r") as f:
             reader = csv.reader(f)
             for row in reader:
-                print(row)
+                function(row)
 
-x = CsvFileReader("1.csv")
-x.read()
+###### how to use it ##########
+# x = CsvFileReader("1.csv")
+# a = lambda row : print(row)
+# x.read(a)
+##############################
