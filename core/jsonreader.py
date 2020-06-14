40b87fb33082ea4c41775fa1e06afffc6ca1624b
jeremy <jeremy@jeremys-MacBook-Pro.local>
2020-06-14 14:01:51 +0800

    Json reader

---
+++ core/jsonreader.py
@@ -0,0 +1,31 @@
+import json
+
+class JsonReader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read(self, chunksize=1024):
+        try:
+            with open(self.filename, "r") as jsonfile:
+                jsondata = json.load(jsonfile)
+                self.readdata(jsondata)
+        except ValueError as e:
+            return False
+        return True
+         
+    def readdata(self, jsondata):
+        for jdx in jsondata:
+            print(jdx)
+            self.readjsondata(jsondata[jdx])
+
+    def readjsondata(self, jsondata):
+        for cjdx in jsondata: 
+                if isinstance(cjdx, dict):
+                    print("more dict")
+                elif isinstance(cjdx, list):
+                    print("list")
+                print(type(cjdx))
+                print(cjdx)
+ 
+x = JsonReader("2.json")
+x.read()
